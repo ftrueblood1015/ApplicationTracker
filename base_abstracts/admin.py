bd1823f2c388e4585f2fb8c527c93e1546571ff2
f.trueblood1015 <f.trueblood1015@gmail.com>
2023-04-29 22:27:06 -0700

    abstracts, overrides, and additional user info

--- base_abstracts/admin.py
+++ base_abstracts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.apps import apps
+from django.conf import settings
+
 
 # Register your models here.
+class ListAdminMixin(object):
+    def __init__(self, admin_model, admin_site):
+        self.list_display = [field.name for field in admin_model._meta.fields]
+        super(ListAdminMixin, self).__init__(model, admin_site)
+
+
+models = apps.get_models()
+for model in models:
+    admin_class = type('AdminClass', (ListAdminMixin, admin.ModelAdmin), {})
+    try:
+        admin.site.register(model, admin_class)
+    except admin.sites.AlreadyRegistered:
+        pass
--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+from base_abstracts.models import TrackingModel
+
+
 # Create your models here.
+class AdditionalUserInfo(TrackingModel):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    phone_number = models.CharField(max_length=12)
+
+    class Meta:
+        verbose_name = 'Additional User Info'
+        verbose_name_plural = 'Additional Users Info'
